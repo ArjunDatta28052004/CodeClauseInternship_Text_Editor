11c065040c519ab1c1678a6f674edfbcffbaf7c3
Arjun Datta <121390516+ArjunDatta28052004@users.noreply.github.com>
2024-01-29 11:23:25 +0530

    Add files via upload

---
+++ TextEditor.py
@@ -0,0 +1,85 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
+class TextEditor:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Text Editor")
+        self.text_area = tk.Text(self.master, undo=True)
+        self.text_area.pack(expand=True, fill="both")
+        self.create_menu()
+
+    def create_menu(self):
+        self.menu_bar = tk.Menu(self.master)
+        
+        # File Menu
+        self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.file_menu.add_command(label="New", command=self.new_file)
+        self.file_menu.add_command(label="Open", command=self.open_file)
+        self.file_menu.add_command(label="Save", command=self.save_file)
+        self.file_menu.add_command(label="Save As", command=self.save_as_file)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Exit", command=self.exit_app)
+        self.menu_bar.add_cascade(label="File", menu=self.file_menu)
+
+        # Edit Menu
+        self.edit_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.edit_menu.add_command(label="Undo", command=self.text_area.edit_undo)
+        self.edit_menu.add_command(label="Redo", command=self.text_area.edit_redo)
+        self.edit_menu.add_separator()
+        self.edit_menu.add_command(label="Cut", command=self.cut_text)
+        self.edit_menu.add_command(label="Copy", command=self.copy_text)
+        self.edit_menu.add_command(label="Paste", command=self.paste_text)
+        self.edit_menu.add_separator()
+        self.edit_menu.add_command(label="Select All", command=self.select_all_text)
+        self.menu_bar.add_cascade(label="Edit", menu=self.edit_menu)
+
+        self.master.config(menu=self.menu_bar)
+
+    def new_file(self):
+        self.text_area.delete(1.0, tk.END)
+
+    def open_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
+        if file_path:
+            with open(file_path, "r") as file:
+                self.text_area.delete(1.0, tk.END)
+                self.text_area.insert(tk.END, file.read())
+
+    def save_file(self):
+        if messagebox.askyesno("Save File", "Do you want to save this file?"):
+            file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt")])
+            if file_path:
+                with open(file_path, "w") as file:
+                    file.write(self.text_area.get(1.0, tk.END))
+
+    def save_as_file(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt")])
+        if file_path:
+            with open(file_path, "w") as file:
+                file.write(self.text_area.get(1.0, tk.END))
+
+    def exit_app(self):
+        if messagebox.askyesno("Exit", "Are you sure you want to exit?"):
+            self.master.destroy()
+
+    def cut_text(self):
+        self.text_area.event_generate("<<Cut>>")
+
+    def copy_text(self):
+        self.text_area.event_generate("<<Copy>>")
+
+    def paste_text(self):
+        self.text_area.event_generate("<<Paste>>")
+
+    def select_all_text(self):
+        self.text_area.tag_add("sel", "1.0", tk.END)
+        return "break"
+
+def main():
+    root = tk.Tk()
+    app = TextEditor(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
